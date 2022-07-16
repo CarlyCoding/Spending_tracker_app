32b6fac04977a3608bf5aa5917013bc3b6969e87
CarlyCoding <carly_off@outlook.com>
2022-07-16 17:33:58 +0100

    classes defined in models

---
+++ models/spend_type.py
@@ -0,0 +1,5 @@
+class Spend_type:
+    def __init__(self, _id = None):
+        self.id = _id 
+        self.spend_type_options = []
+
---
+++ models/transaction.py
@@ -0,0 +1,8 @@
+class Transaction:
+    def __init__(self, _description, _amount, _date, _id = None):
+        self.description = _description
+        self.amount = _amount
+        self.date = _date 
+        self.id = _id
+
+
