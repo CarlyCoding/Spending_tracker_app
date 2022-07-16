908f5467f06b15cf35ce18af5de2a858409547f3
CarlyCoding <carly_off@outlook.com>
2022-07-16 16:21:54 +0100

    folder structures added

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+from controllers.tasks_controller import tasks_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(tasks_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
