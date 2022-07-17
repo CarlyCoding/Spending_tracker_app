c22956368fd6b4ae9ddd7951523964b91a14f2a9
CarlyCoding <carly_off@outlook.com>
2022-07-17 18:12:24 +0100

    attempt 1 at sql

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -3,5 +3,12 @@ DROP TABLE IF EXISTS merchants;
 
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
-    
+    _description VARCHAR(255),
+    _amount DECIMAL(10,2),
+    _date DATETIME 
+)
+
+CREATE TABLE merchants(
+    id SERIAL PRIMARY KEY,
+    _merchant_name = VARCHAR(255),
 )
