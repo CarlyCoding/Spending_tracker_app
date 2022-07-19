43ff72bc3dcc9da2a135c2289868ba79ef8617d1
CarlyCoding <carly_off@outlook.com>
2022-07-19 11:12:55 +0100

    merchant repo being populated

--- controllers/merchant_controller.py
+++ controllers/merchant_controller.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint 
 from models import merchant
@@ -6,12 +7,40 @@ import repositories.merchant_repository as merchant_repository
 import repositories.transaction_repository as transaction_repository
 import repositories.type_repository as type_repository
 
-merchant_blueprint = Merchant("merchant", __name__)
+merchants_blueprint = Merchant("merchant", __name__)
 
+merchants_blueprint.route("/merchants")
+def merchants():
+    merchants = merchant_repository.select_all()
+    return render_template("pages/merchants.html", all_merchants = merchants)
+    
+# GET 'merchants/new'
+@merchants_blueprint.route("/merchants/new", methods= ['GET'])
+def new_merchant():
+    # put in the main code about merchants here. 
+    return render_template("merchants/new.html", all_merchants = merchants)
+# This block needs more work
 
-# GET
 # CREATE
+# POST 'merchants'
+@merchants_blueprint.route("/merchants", methods= ['POST'])
+def create_transaction():
+    name = request.form['name']
+    merchant_repository.save(merchant)
+    return redirect('/merchants')
+
 # SHOW
+# GET '/merchants'
+@merchants_blueprint.route('/merchants/<id>', methods= ['GET'])
+def show_merchant(id):
+    merchant = merchant_repository.select(id)
+    return render_template('merchants/show.html', merchant = merchant)
+
 # EDIT
+# GET '/merchants'
+
 # UPDATE 
+# Put something here for updating a merchant
+
 # DELETE 
+# For deleting a merchant 
